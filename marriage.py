bc65bc36b5cb242b16aacf22c529846c96842044
Jake <jporetsky14@gmail.com>
2020-02-05 15:17:48 -0500

    new stuff
    nnew stuff

--- marriage.py
+++ marriage.py
@@ -10,32 +10,37 @@ def AssemblePreferences(fileName):
     #Open the file and put the preferences in dictionaries
     with open(fileName) as f:
         for line in f:
-            if(lineCount == 0): n = int(line)
-            lineCount+=1
-            line = line.strip()
-            choices = line.split(" ")
-            if(lineCount > 1 and lineCount <= n+1):
-                #Make the knight the key
-                current_knight = choices.pop(0)
-                #Make his preference list of ladies the values
-                KnightPrefs[current_knight] = choices
-            elif (lineCount > n):
-                #Make the knight the key
-                current_lady = choices.pop(0)
-                #Make his preference list of ladies the values
-                LadyPrefs[current_lady] = choices
+            ####### INITIALIZE IMPORTANT VAR's  #######
+            lineCount+=1 # tracks line number.
+            line = line.strip() #removes newline char from each line
+            choices = line.split(" ") #Makes an array with elements being each name on the line
+            if(lineCount == 1 and len(choices)==1): n = int(line)   #base case. finds value of n
 
-    print("Knights: ", KnightPrefs)
-    print("Ladies: ", LadyPrefs)
+            #######  ERROR CHECKING  #######
+            elif(lineCount == 1 and len(choices)!=1): exit(1) # ensures line 1 always has 1 value
+            if(lineCount>1 and len(choices) != n+1): exit(1) #Check for file format error (num of names per line)
+            
 
-    return KnightPrefs, LadyPrefs
+            #######  BUILD DICTIONARY  #######
+            if(lineCount > 1 and lineCount <= n+1): # Knights dictionary contstruction
+                current_knight = choices.pop(0) #Make the knight the key
+                KnightPrefs[current_knight] = choices #Make his preference list of ladies the values
+            elif (lineCount > n+1): # Lady dictionary construction
+                current_lady = choices.pop(0) #Make the lady the key
+                LadyPrefs[current_lady] = choices #Make her preference list of knights the values
+
+
+    if(lineCount != (2*n)+1): exit(1) #checks for instance of file not having correct num of lines.
+    
+    print("Knights: ", KnightPrefs) # FOR TESTING
+    print("Ladies: ", LadyPrefs) # FOR TESTING
+
+    return KnightPrefs, LadyPrefs # returns dictionaries
 
 def propose(kPrefs, lPrefs):
-    for i in range(0, kPrefs[0].length()):
-        print(i)
-
 
 def main():
     kPrefs, lPrefs = AssemblePreferences(sys.argv[1])
+    
 
 main()
