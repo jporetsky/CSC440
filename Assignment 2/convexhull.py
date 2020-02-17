99b23974ef1c53597bef6eed0566db1d33012ee4
Jake <jporetsky14@gmail.com>
2020-02-17 14:59:36 -0500

    Update convexhull.py

--- Assignment 2/convexhull.py
+++ Assignment 2/convexhull.py
@@ -86,19 +86,20 @@ def merge(left, right):
 	
 	### yint args for left ###
 	lp = left[i]  # current point in left hull
-	lp1 = left[i-1] 
-	lp2 = left[i+1]
+	lp1 = left[i-1] # finds counter-clockwise point in left
+	lp2 = left[i+1] # finds clockwise point in left (currently unused)
 
-	ly = left[i][1]
-	ly3 = left[i-1][1]
-	ly4 = left[i+1][1]
+	ly = left[i][1] # y val of current left point
+	ly3 = left[i-1][1] # y of counter-clockwise point in left
+	ly4 = left[i+1][1] # y of clockwise point in left (currently unused)
+
 	### yint args for right ###
 	rp = right[j]  # current point in right hull
-	rp1 = right[j-1]
+	rp1 = right[j-1] # (currently unused)
 	rp2 = right[j+1]
 
-	ry = right[j][1]
-	ry3 = right[j-1][1]
+	ry = right[j][1] # y val of current right point
+	ry3 = right[j-1][1] # (currently unused)
 	ry4 = right[j+1][1]
 
 	### yint args for x ###
@@ -106,6 +107,7 @@ def merge(left, right):
 	xplusR = lp[0] + ((rp2[0] - lp[0])/2)  # x val of tangent with point right[j+1] 
 	xminusL = lp1[0] + ((rp[0] - lp1[0])/2) # x val of tangent with point left[i-1] 
 
+	### yint func returns tuples representing coords of y tangent (x,y) ###
 	yTang = yint(lp, rp, x, ly, ry) # finds y tangent for current left and right points
 	yTangplusR = yint(lp, rp2, xplusR, ly, ry2) # finds yTang with point right[j+1]
 	yTangminusL = yint(lp2, rp1, xminusL, ly3, rp4) # finds yTang with point right[i-1]
@@ -119,17 +121,17 @@ def merge(left, right):
 		lp1 = left[i-1] # finds counter-clockwise point in left
 		lp2 = left[i+1] # finds clockwise point in left (currently unused)
 
-		ly = left[i][1] # y val of current point
+		ly = left[i][1] # y val of current left point
 		ly3 = left[i-1][1] # y of counter-clockwise point in left
 		ly4 = left[i+1][1] # y of clockwise point in left (currently unused)
 
 		### yint args for right ###
 		rp = right[j]  # current point in right hull
-		rp1 = right[j-1] 
+		rp1 = right[j-1] # (currently unused)
 		rp2 = right[j+1]
 
-		ry = right[j][1]
-		ry3 = right[j-1][1]
+		ry = right[j][1] # y val of current right point
+		ry3 = right[j-1][1] # (currently unused)
 		ry4 = right[j+1][1]
 
 		### yint args for x ###
@@ -141,7 +143,7 @@ def merge(left, right):
 		yTang = yint(lp, rp, x, ly, ry) # finds y tangent for current left and right points
 		yTangplusR = yint(lp, rp2, xplusR, ly, ry2) # finds yTang with point right[j+1]
 		yTangminusL = yint(lp2, rp1, xminusL, ly3, rp4) # finds yTang with point right[i-1]
-		
+
 #########################################################################################################################
 
 		if (yint(left[i], right[j+1], left[i][0], left[i][1], right[j+1][1]) > yint(left[i], right[j], left[i][0], left[i][1], right[j][1])):	#move right finger clockwise
